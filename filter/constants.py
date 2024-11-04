e54bc74f988b6a97b72a948294d47f1d53b51f73
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-11-04 18:59:45 +0900

    fix: supplement ASCII filter logic

--- filter/constants.py
+++ filter/constants.py
@@ -9,7 +9,13 @@ ARROWS = ['→', '←', '↑', '↓', '↔']
 
 # 컬럼명에 대한 상수 정의
 COLUMN_TEXT = 'text'
+COLUMN_LENGTH = 'length'
+COLUMN_ASCII_COUNT = 'ascii_count'
+COLUMN_IS_ENGLISH_ONLY = 'is_english_only'
+COLUMN_IS_ALL_UPPERCASE = 'is_all_uppercase'
 COLUMN_ASCII_RATIO = 'ascii_ratio'
+COLUMN_ASCII_UPPERCASE_RATIO = 'ascii_uppercase_ratio'
+COLUMN_ENGLISH_UPPERCASE_RATIO = 'english_uppercase_ratio'
 COLUMN_ENGLISH_COUNT = 'english_count'
 COLUMN_SPECIAL_CHAR_COUNT = 'special_char_count'
 COLUMN_SPECIAL_CHAR_RATIO = 'special_char_ratio'
